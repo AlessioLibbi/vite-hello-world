f4384378c9cabe46ec3a7f2c5bee3fffd511fe2c
Alessio Libbi <alex.libbi@gmail.com>
2023-04-12 13:43:18 +0200

    Add: exercise with bonus

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: "AppJumbo",
+    data() {
+        return {
+            imgText: "Sono il jumbotron"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbo-cont">
+        <h2 class="jumb-title">{{ imgText }}</h2>
+        <img class="img-jumb" src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style>
+.jumbo-cont {
+    background-color: blue;
+    margin: 0 auto;
+}
+
+.jumb-title {
+    font-size: 4rem;
+    justify-content: center;
+    color: white;
+    display: flex;
+    padding: 20px;
+}
+
+.img-jumb {
+    width: 100%;
+    height: 300px;
+
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+
+        <h1 class="main-title">{{ title }}</h1>
+    </div>
+</template>
+
+<style>
+.container {
+    width: 90%;
+    margin: 0 auto;
+}
+
+.main-title {
+    color: blue;
+    font-size: 5rem;
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+}
+</style>
